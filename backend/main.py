b5cf2a490b3216b45f5a26b59a8aa1bc87687aab
Michael Shahine <145465112+MichaelSH7@users.noreply.github.com>
2023-11-28 19:12:20 +0000

    second issue

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,27 @@
 from fastapi import FastAPI 
 from typing import Union
 from movies import Movies
+from movies import Movie
 
 moviesApp = FastAPI()
 
-movies = Movies('./movies.txt')
+movies = Movies('./movies.txt') 
 
 @moviesApp.get("/movies/{movie_id}")
-def Finding_Movie_id(movie_id: str):
-    if 0 <= int(movie_id) < len(movies._movies):
-        return movies._movies[int(movie_id)]
-    else:
-        return None
+def Finding_Movie_id(movie_id: int) -> Union[Movie, None]:
+    for i in range(0,len(movies._movies),1):
+        if (movies._movies[i]['id']==movie_id):
+            return movies._movies[i]
+# update?
+
+@moviesApp.put("/movies/{movie_id}")
+def add_movie(movie_id: int, new_movie: Movie)-> Union[Movie, None]:
+    for i in range (0, len(movies._movies),1):
+        if(movies._movies[i]['id']==movie_id):
+            movies._movies [i]['name'] = new_movie.name
+            movies._movies [i]['cast'] = new_movie.cast
+            return {
+                "id": new_movie.id,
+                    "name": new_movie.name,
+                    "cast": new_movie.cast
+            }
--- backend/movies.py
+++ backend/movies.py
@@ -1,7 +1,25 @@
+from pydantic import BaseModel
+from typing import Any
+from typing import List, Set
+
+class new_movie:
+    movie_name: str
+    movie_cast: set[str]
+    
+class Movie(BaseModel):
+    id: int
+    name: str
+    cast: List[str]
+
 class Movies:
+#class Movies:
+
+    #_movies: List[ dict[str,List[str]] ]
+    
     def __init__(self, movies_file):
         self._movies = []
 
+        count = 0 
         with open(movies_file, encoding="utf-8") as file:
             row_idx = 0
             for line in file:
@@ -10,63 +28,16 @@ class Movies:
                 if row_idx%3 == 1:
                     movie_cast = line.rstrip().split(',')
                 if row_idx%3 == 2:
+                   
                     self._movies.append(
                         {
+                            'id'  : count,
                             'name': movie_name,
                             'cast': movie_cast
                         }
                     )
-                    movie_name = None
-                    movie_cast = None
+                    count += 1
                 row_idx += 1
 
-        if movie_name and movie_cast:
-            # Add the last movie to the list
-            self._movies.append(
-                {
-                    'name': movie_name,
-                    'cast': movie_cast
-                }
-            )
-            
-    def search_movies_by_name(self, keyword):
-        matched_movies = []
-        for movies in self._movies:
-            if keyword.lower() in movies['name'].lower():
-                matched_movies.append(movies['name'])
-
-        if matched_movies:
-            for movie_name in matched_movies:
-                print(movie_name)
-        else:
-            print("No matching movies found.")
-
-    def list_all_movies(self) :
-        for movies in self._movies:
-            print(movies['name'])
-
-
-    def search_movies_by_cast(self, keyword):
-        matched_movies = []
-
-        for movies in self._movies:
-            matched_cast = []
-            for actor in movies['cast']:
-                if keyword.lower() in actor.lower():
-                    matched_cast.append(actor)
-            if matched_cast:
-                matched_movies.append({
-                    'name': movies['name'],
-                    'cast': matched_cast
-                })
-
-        if matched_movies:
-            for movies in matched_movies:
-                print(movies['name'])
-                print(movies['cast'])
-        else:
-            print("\nNo movies matched the search keyword")
-            
-
 if __name__ == "__main__":
-    movies = Movies('./movies.txt')
+    myMovies = Movies('./movies.txt')
